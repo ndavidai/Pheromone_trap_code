814d61ebc9adae7d67968b6cb3b5e3fd0f0701e3
SEvanGF <evan.gonzalez2901@gmail.com>
2024-10-07 15:17:16 -0400

    Creating 2024 code folder

---
+++ 2024_code/scripts/Packages.R
@@ -0,0 +1,20 @@
+##################################
+########### PACKAGES #############
+##################################
+
+#Run without the '#' to install the packages 
+
+#install.packages(c("janitor","dplyr","tidyverse","ggplot2","forcats","foreign","nnet","reshape2","esquisse"))
+
+#Packages to load
+#There is code at the top of each script that runs this script which will load packages needed
+
+library(dplyr)
+library(janitor)
+library(tidyverse)
+library(ggplot2)
+library(forcats)
+library(foreign)
+library(nnet)
+library(reshape2)
+library(esquisse)
