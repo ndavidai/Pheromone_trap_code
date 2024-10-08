4d352404fe94091eed583c0e9e99ad27c3776721
SEvanGF <evan.gonzalez2901@gmail.com>
2024-10-08 16:03:22 -0400

    Small comment fixes

--- 2024_code/scripts/2024_Data_Cleaning.R
+++ 2024_code/scripts/2024_Data_Cleaning.R
@@ -10,7 +10,7 @@ source("2024_code/scripts/Packages.R")
 
 moth_counts_unedited <- read.csv("2024_code/input/Moth_count_data_2024.csv")
 
-# library(janitor) janitor cleans up column names.It removes all unique characters and replaces spaces with _.
+# library(janitor) cleans up column names.It removes all unique characters and replaces spaces with _.
 # piping using `dplyr`
 
 moth_counts_cleannames <- moth_counts_unedited %>%
@@ -30,8 +30,8 @@ moth_counts_pineoakfix <- moth_counts_cleannames %>%
 
 unique(moth_counts_pineoakfix$stand_type)
 
-#remove all spaces
-## in order to standardize all patch names, remove all spaces and replace with _'s
+
+# in order to standardize all patch names, remove all spaces and replace with _'s
 
 moth_counts_clean <- moth_counts_pineoakfix %>%
   mutate(patch_name = str_replace_all(patch_name, " ","_"))
