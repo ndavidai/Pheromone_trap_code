82cad2cdcb13408decceaabacb190becc1d051bd
Samuel Evan Gonzalez-Fleurant (7 Gluten Free Bread Loafs) <evan.gonzalez2901@gmail.com>
2024-10-09 14:16:28 -0400

    removed code no longer needed (commented)

--- 2024_code/scripts/2024_Data_Cleaning.R
+++ 2024_code/scripts/2024_Data_Cleaning.R
@@ -10,30 +10,30 @@ source("2024_code/scripts/Packages.R")
 
 moth_counts_unedited <- read.csv("2024_code/input/Moth_count_data_2024.csv")
 
-# library(janitor) cleans up column names.It removes all unique characters and replaces spaces with _.
-# piping using `dplyr`
+# # library(janitor) cleans up column names.It removes all unique characters and replaces spaces with _.
+# # piping using `dplyr`
+# 
+# moth_counts_cleannames <- moth_counts_unedited %>%
+#   clean_names() #Cleans names of an object (usually a data.frame)
 
-moth_counts_cleannames <- moth_counts_unedited %>%
-  clean_names() #Cleans names of an object (usually a data.frame)
+# # if any column names need replacing
+# colnames(moth_counts_cleannames)[colnames(moth_counts_cleannames)=="x_muck_mass_g"] <- "muck_mass"
 
-# if any column names need replacing
-colnames(moth_counts_cleannames)[colnames(moth_counts_cleannames)=="x_muck_mass_g"] <- "muck_mass"
+# # looking for mistakes
+# unique(moth_counts_cleannames$stand_type)
+# unique(moth_counts_cleannames$patch_name)
 
-# looking for mistakes
-unique(moth_counts_cleannames$stand_type)
-unique(moth_counts_cleannames$patch_name)
-
-#above code found there to be 'oak' and 'Oak', same for pine. Following code replaces pine/oak with Pine/Oak
-moth_counts_pineoakfix <- moth_counts_cleannames %>%
-  mutate(stand_type = str_replace_all(stand_type, 'pine','Pine'))%>%
-  mutate(stand_type = str_replace_all(stand_type, 'oak', 'Oak'))
-
-unique(moth_counts_pineoakfix$stand_type)
+# #above code found there to be 'oak' and 'Oak', same for pine. Following code replaces pine/oak with Pine/Oak
+# moth_counts_pineoakfix <- moth_counts_cleannames %>%
+#   mutate(stand_type = str_replace_all(stand_type, 'pine','Pine'))%>%
+#   mutate(stand_type = str_replace_all(stand_type, 'oak', 'Oak'))
+# 
+# unique(moth_counts_pineoakfix$stand_type)
 
 
 # in order to standardize all patch names, remove all spaces and replace with _'s
 
-moth_counts_clean <- moth_counts_pineoakfix %>%
+moth_counts_clean <- moth_counts_unedited %>%
   mutate(patch_name = str_replace_all(patch_name, " ","_"))
 
 # quick visualizations if needed
