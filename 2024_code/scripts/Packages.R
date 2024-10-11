7427c6e5d2bef9edcc5dfa6d769e63e62a25ecbf
Samuel Evan Gonzalez-Fleurant (7 Gluten Free Bread Loafs) <evan.gonzalez2901@gmail.com>
2024-10-11 15:02:25 -0400

    adding more packages

--- 2024_code/scripts/Packages.R
+++ 2024_code/scripts/Packages.R
@@ -4,10 +4,10 @@
 
 #Run without the '#' to install the packages 
 
-#install.packages(c("janitor","dplyr","tidyverse","ggplot2","forcats","foreign","nnet","reshape2","esquisse", "summarytools", "lme4", "tidyr","plotrix"))
+#install.packages(c("janitor","dplyr","tidyverse","ggplot2","forcats","foreign","nnet","reshape2","esquisse", "summarytools", "lme4", "tidyr","plotrix","MetBrewer","viridis"))
 
 #Packages to load
-#There is code at the top of each script that runs this script which will load packages needed
+#There is code at the top of 2024_Data_Cleaning.R that runs these packages
 
 library(dplyr)
 library(janitor)
@@ -21,6 +21,16 @@ library(esquisse)
 library(summarytools)
 library(lme4)
 library(tidyr)
-library(plotrix)
+library(plotrix) #used for std.error
 library(sjPlot)
+library(MetBrewer)#nice colourblind friendly options
+library(viridis)#nice colourblind friendly options
 
+
+
+#####
+
+#Additional: Colourblind package obtained from the rcartocolor package (not installed)
+
+safe_colorblind_palette_edited <- c("#88CCEE", "#CC6677", "#DDCC77", "#117733", "#332288", "#AA4499", 
+                             "#44AA99", "#661100") #HEX CODES OBTAINED FROM rcartocolor package
