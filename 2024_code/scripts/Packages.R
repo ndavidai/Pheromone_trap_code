6cc8ed7f309f983e9954f16ed9e459e8a7be1587
Samuel Evan Gonzalez-Fleurant (7 Gluten Free Bread Loafs) <evan.gonzalez2901@gmail.com>
2024-10-18 23:03:08 -0400

    Update Packages.R

--- 2024_code/scripts/Packages.R
+++ 2024_code/scripts/Packages.R
@@ -4,7 +4,7 @@
 
 #Run without the '#' to install the packages 
 
-#install.packages(c("janitor","dplyr","tidyverse","ggplot2","forcats","foreign","nnet","reshape2","esquisse", "summarytools", "lme4", "tidyr","plotrix","MetBrewer","viridis","performance","car","effects","MASS","mgcv","Matrix"))
+#install.packages(c("janitor","dplyr","tidyverse","ggplot2","forcats","foreign","nnet","reshape2","esquisse", "summarytools", "lme4", "tidyr","plotrix","MetBrewer","viridis","performance","car","effects","MASS","mgcv","Matrix","MetBrewer"))
 
 #Packages to load
 #There is code at the top of 2024_Data_Cleaning.R that runs these packages
@@ -33,6 +33,7 @@ library(mgcv) #install.packages("mgcv")
 library(Matrix)
 library(car)
 library(effects)
+library(MetBrewer)
 
 #####
 
